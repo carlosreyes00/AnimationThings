ef900794b6c22e14850028656c1da3e026a07a0e
Carlos Reyes <crrmagadan@icloud.com>
2023-07-27 15:55:17 -0400

    Initial Commit

---
+++ AnimationThings/AnimationThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationThingsApp.swift
+//  AnimationThings
+//
+//  Created by Carlos Rafael Reyes Magadán on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationThings/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimationThings
+//
+//  Created by Carlos Rafael Reyes Magadán on 7/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
